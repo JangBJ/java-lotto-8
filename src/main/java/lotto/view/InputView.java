4453fb6b15c4f88ce6318a6552a864aa72444aa9
JangBJ <qudwnd1001@naver.com>
2025-11-03 14:46:48 +0900

    feat: 입력 안내문 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,15 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String GET_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String GET_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public String getPrice(){
+        System.out.println(GET_MONEY_MESSAGE);
         return Console.readLine();
     }
 
     public String getWinningLottoNumber(){
+        System.out.println(GET_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
     public String getBonusLottoNumber(){
+        System.out.println(GET_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
