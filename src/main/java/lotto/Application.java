8906f69a5d145bfcd1a3a9cd4e40ce2f9c82872e
JangBJ <qudwnd1001@naver.com>
2025-11-03 14:48:26 +0900

    feat: mvp1 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.domain.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        LottoGame lottoGame = new LottoGame(new InputView(), new OutputView(), new LottoMachine());
+
+        lottoGame.start();
     }
 }
