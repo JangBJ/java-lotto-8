bbfe800d1e07effc83d58b89716d0118c7c31ddb
JangBJ <qudwnd1001@naver.com>
2025-11-03 13:40:01 +0900

    feat: 로또 번호 객체 생성

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoNumber {
+
+    private final List<Integer> lottoNumber;
+
+    public LottoNumber(List<Integer> lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+
+}
