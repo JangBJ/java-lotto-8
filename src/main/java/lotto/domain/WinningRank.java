ac6f9c25f44d3b21767715fb4de6b7163d1fb07b
JangBJ <qudwnd1001@naver.com>
2025-11-03 19:27:33 +0900

    refactor: 3항 연산자 -> case문 변경

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -2,11 +2,11 @@ package lotto.domain;
 
 public enum WinningRank {
     FIRST (6, false, 2_000_000_000L),
-    SECOND(5, true,     30_000_000L),
-    THIRD (5, false,     1_500_000L),
-    FOURTH(4, false,        50_000L),
-    FIFTH (3, false,         5_000L),
-    MISS  (0, false,              0L);
+    SECOND(5, true, 30_000_000L),
+    THIRD (5, false, 1_500_000L),
+    FOURTH(4, false, 50_000L),
+    FIFTH (3, false, 5_000L),
+    MISS  (0, false, 0L);
 
     public final int match;
     public final boolean bonus;
@@ -18,11 +18,18 @@ public enum WinningRank {
         this.prizeMoney = prize;
     }
 
-    public static WinningRank of(int matchCount, boolean bonusMatched) {
-        if (matchCount == 6) return FIRST;
-        if (matchCount == 5) return bonusMatched ? SECOND : THIRD;
-        if (matchCount == 4) return FOURTH;
-        if (matchCount == 3) return FIFTH;
-        return MISS;
+    public static WinningRank of(int matchCount, boolean bonusMatch) {
+        switch (matchCount) {
+            case 6: return FIRST;
+            case 5: return rankForFive(bonusMatch);
+            case 4: return FOURTH;
+            case 3: return FIFTH;
+            default: return MISS;
+        }
+    }
+
+    private static WinningRank rankForFive(boolean bonusMatch) {
+        if(bonusMatch) return SECOND;
+        return THIRD;
     }
 }
