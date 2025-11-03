4811d9a61a546a20e1f80ce2aa1f221da70501f0
JangBJ <qudwnd1001@naver.com>
2025-11-03 19:40:27 +0900

    feat: mvp2 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,8 +1,7 @@
 package lotto;
 
-import lotto.domain.LottoMachine;
-import lotto.domain.LottoNumber;
-import lotto.domain.Money;
+import lotto.domain.*;
+import lotto.parse.InputParser;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -21,16 +20,27 @@ public class LottoGame {
         this.lottoMachine = lottoMachine;
     }
 
+    public void start() {
+        int money = Integer.parseInt(inputView.getPrice().trim());
+        Money purchase = new Money(money);
+        int count = purchase.moneyToPriceCount();
 
-    public void start(){
-        int money  = Integer.parseInt(inputView.getPrice());
-        Money moneyCount = new Money(money);
-        int count = moneyCount.moneyToPriceCount();
-        inputView.getWinningLottoNumber();
-        inputView.getBonusLottoNumber();
+        List<LottoNumber> tickets = lottoMachine.createRandomLottoList(count);
+        outputView.printLottoList(tickets);
+        System.out.println();
 
-        List<LottoNumber> lottoNumberList = lottoMachine.createRandomLottoList(count);
+        String winningLine = inputView.getWinningLottoNumber();
+        String bonusLine = inputView.getBonusLottoNumber();
 
-        outputView.printLottoList(lottoNumberList);
+        WinningLotto winning = new WinningLotto(
+                InputParser.winningNumberParser(winningLine),
+                InputParser.bonusNumberParser(bonusLine)
+        );
+
+        LottoJudge judge = new LottoJudge();
+        LottoResult result = judge.winningJudge(tickets, winning);
+
+        outputView.printStatistics(result);
+        outputView.printProfitPercent(result, money);
     }
 }
